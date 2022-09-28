ceb774272c73d11be14e65cc6c909d5993f20f5e
Arash Afshar <arash.afshar.edu@gmail.com>
2022-09-27 18:14:45 -0600

    use a template chrome extension

---
+++ extension/src/pages/Popup/Popup.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { ChromeMessage, Sender } from "../types";
+import { getCurrentTabUId, getCurrentTabUrl } from "../utils";
+
+const Popup = () => {
+  const [url, setUrl] = useState<string>('');
+  const [responseFromContent, setResponseFromContent] = useState<string>('');
+
+
+  /**
+   * Get current URL
+   */
+  useEffect(() => {
+    getCurrentTabUrl((url) => {
+      setUrl(url || 'undefined');
+    })
+  }, []);
+
+  const sendTestMessage = () => {
+    const message: ChromeMessage = {
+      from: Sender.React,
+      message: "Hello from React",
+    }
+
+    getCurrentTabUId((id) => {
+      id && chrome.tabs.sendMessage(
+        id,
+        message,
+        (responseFromContentScript) => {
+          setResponseFromContent(responseFromContentScript);
+        });
+    });
+  };
+
+  const sendRemoveMessage = () => {
+    const message: ChromeMessage = {
+      from: Sender.React,
+      message: "delete logo",
+    }
+
+    getCurrentTabUId((id) => {
+      id && chrome.tabs.sendMessage(
+        id,
+        message,
+        (response) => {
+          setResponseFromContent(response);
+        });
+    });
+  };
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Home</p>
+        <p>URL:</p>
+        <p>
+          {url}
+        </p>
+        <button onClick={sendTestMessage}>SEND MESSAGE</button>
+        <button onClick={sendRemoveMessage}>Remove logo</button>
+        <p>Response from content:</p>
+        <p>
+          {responseFromContent}
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default Popup;
